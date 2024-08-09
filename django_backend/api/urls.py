20f2c70e117c42d9cf4beb180d1fc568459762b7
BadrRibzat <badrribzat@gmail.com>
2024-08-09 17:06:14 +0100

    Update URL patterns to include new login view

--- django_backend/api/urls.py
+++ django_backend/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .views import (
     UserViewSet, LevelViewSet, LessonViewSet, FlashcardViewSet,
     UserProgressViewSet, QuizViewSet, AchievementViewSet, UserAchievementViewSet,
-    chatbot, RegisterView, visitor_lessons, UserProfileView
+    chatbot, RegisterView, LoginView, UserProfileView
 )
 
 router = DefaultRouter()
@@ -21,9 +20,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('chatbot/', chatbot, name='chatbot'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('lessons/visitor/', visitor_lessons, name='visitor_lessons'),
+    path('login/', LoginView.as_view(), name='login'),
     path('user/profile/', UserProfileView.as_view(), name='user_profile'),
 ]
